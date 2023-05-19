cdad07dbe3d5827e17fe14caa24ae1adca015f4a
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-05-18 23:26:55 -0300

    ajustes
    função mudaLocal estava incorreto
    vetor cobra alterado para [0] em caráter de teste

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var cobraX = (tamanho * 5)
 var cobraY = (tamanho * 5)
 var velocidadeX = 0
 var velocidadeY = 0
-var cobra = [4]
+var cobra = [0]
 var macaX
 var macaY
 var fim = false
@@ -30,35 +30,48 @@ function atualiza() {
     if (fim) {
         return
     }
-        conteudo.fillStyle = "black"
-        conteudo.fillRect(0, 0, tela.width, tela.height)
 
-        conteudo.fillStyle = "red"
-        conteudo.fillRect(macaX, macaY, tamanho, tamanho)
+    conteudo.fillStyle = "black"
+    conteudo.fillRect(0, 0, tela.width, tela.height)
 
-        if (cobraX == macaX && cobraY == macaY) {
-            cobra.push = ([macaX, macaY])
-            comida()
-        }
+    conteudo.fillStyle = "red"
+    conteudo.fillRect(macaX, macaY, tamanho, tamanho)
 
-        for (i = cobra.length - 1; i > 0; i--) {
-            cobra[i] = cobra[i - 1]
-        }
-        if (cobra.length) {
-            cobra[4] = [cobraX, cobraY]
+    if (cobraX == macaX && cobraY == macaY) {
+        cobra.push = ([macaX, macaY])
+        comida()
     }
 
-conteudo.fillStyle="blue"
-cobraX+=velocidadeX*tamanho
-cobraY+=velocidadeY*tamanho
-conteudo.fillRect(cobraX, cobraY, tamanho, tamanho)
-for(i = 0; i< cobra.length; i++){
-    conteudo.fillRect(cobra[i][0], cobra[i][1], tamanho, tamanho)
-}
+    for (i = cobra.length - 1; i > 0; i--) {
+        cobra[i] = cobra[i - 1]
+    }
+    if (cobra.length) {
+        cobra[0] = [cobraX, cobraY]
+    }
+
+    conteudo.fillStyle = "blue"
+    cobraX += velocidadeX * tamanho
+    cobraY += velocidadeY * tamanho
+    conteudo.fillRect(cobraX, cobraY, tamanho, tamanho)
+    for (i = 0; i < cobra.length; i++) {
+        conteudo.fillRect(cobra[i][0], cobra[i][1], tamanho, tamanho)
+    }
+    if (cobraX < 0 || cobraX > colunas * tamanho || cobraY < 0 || cobraY > linhas * tamanho) {
+        fim = true
+        alert(`Perdeu, mané!`)
+    }
+
+    for (i = 0; i < cobra.length; i++) {
+        if (cobraX == cobra[i][0] && cobraY == cobra[i][1]) {
+            fim = true
+            alert(`Perdeu, mané!`)
+        }
+    }
+
 }
 
 function mudaLocal(x) {
-    if (x.code == "cima" && velocidadeY != -1) {
+    if (x.code == "cima" && velocidadeY != 1) {
         velocidadeX = 0
         velocidadeY = -1
     } else if (x.code == "baixo" && velocidadeY != -1) {
@@ -67,7 +80,7 @@ function mudaLocal(x) {
     } else if (x.code == "esquerda" && velocidadeX != 1) {
         velocidadeX = -1
         velocidadeY = 0
-    } else if (x.code == "esquerda" && velocidadeX != 1) {
+    } else if (x.code == "direita" && velocidadeX != -1) {
         velocidadeX = 1
         velocidadeY = 0
     }
