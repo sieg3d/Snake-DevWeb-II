a7f8bdb943a0be7e3a31cc6ac14c3d112da81e5b
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-05-19 00:45:43 -0300

    Lapidando
    Funcionando
    Definir o inicio da serpente com 3 blocos

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ var linhas = 20
 var colunas = 20
 var fundo
 var conteudo
-var cobraX = tamanho * 5
-var cobraY = tamanho * 5
+var cobraX = tamanho
+var cobraY = tamanho
 var velocidadeX = 0
 var velocidadeY = 0
 var cobra = []
 var macaX
 var macaY
 var fim = false
+var pontos = -1
 
 window.onload = function () {
     fundo = document.getElementById("tela")
@@ -19,6 +20,9 @@ window.onload = function () {
     conteudo = fundo.getContext("2d")
 
     comida()
+    for (let i = 0; i < 4; i++) {
+        cobra.push([cobraX - tamanho * i, cobraY])
+    }
     document.addEventListener("keyup", direcao)
     setInterval(atualiza, 1000 / 10)
 }
@@ -37,7 +41,7 @@ function atualiza() {
     conteudo.fillStyle = "red"
     conteudo.fillRect(macaX, macaY, tamanho, tamanho)
 
-    if (cobraX == macaX && cobraY == macaY) {
+    if (cobraX === macaX && cobraY === macaY) {
         cobra.push([macaX, macaY])
         comida()
     }
@@ -56,31 +60,31 @@ function atualiza() {
     for (let i = 0; i < cobra.length; i++) {
         conteudo.fillRect(cobra[i][0], cobra[i][1], tamanho, tamanho)
     }
-    if (cobraX < 0 || cobraX > colunas * tamanho || cobraY < 0 || cobraY > linhas * tamanho) {
+    if (cobraX < 0 || cobraX >= colunas * tamanho || cobraY < 0 || cobraY >= linhas * tamanho) {
         fim = true
-        alert(`Perdeu, mané!`)
+        alert(`Perdeu, mané!\nFez ${pontos} pontos. Parabens seus cocô`)
     }
 
     for (i = 0; i < cobra.length; i++) {
-        if (cobraX == cobra[i][0] && cobraY == cobra[i][1]) {
+        if (cobraX === cobra[i][0] && cobraY === cobra[i][1]) {
             fim = true
-            alert(`Perdeu, mané!`)
+            alert(`Perdeu, mané!\nFez ${pontos} pontos. Parabens seus cocô`)
         }
     }
 
 }
 
-function direcao(x) {
-    if (x.code == "ArrowUp" && velocidadeY != 1) {
+function direcao(evento) {
+    if (evento.code === "ArrowUp" && velocidadeY != 1) {
         velocidadeX = 0
         velocidadeY = -1
-    } else if (x.code == "ArrowDown" && velocidadeY != -1) {
+    } else if (evento.code === "ArrowDown" && velocidadeY != -1) {
         velocidadeX = 0
         velocidadeY = 1
-    } else if (x.code == "ArrowLeft" && velocidadeX != 1) {
+    } else if (evento.code === "ArrowLeft" && velocidadeX != 1) {
         velocidadeX = -1
         velocidadeY = 0
-    } else if (x.code == "ArrowRight" && velocidadeX != -1) {
+    } else if (evento.code === "ArrowRight" && velocidadeX != -1) {
         velocidadeX = 1
         velocidadeY = 0
     }
@@ -89,5 +93,7 @@ function direcao(x) {
 function comida() {
     macaX = Math.floor(Math.random() * colunas) * tamanho
     macaY = Math.floor(Math.random() * linhas) * tamanho
+    ++pontos
+    console.log(`${pontos}`)
 }
 
