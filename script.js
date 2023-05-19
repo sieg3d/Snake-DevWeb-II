3f8f753c248d312cd76e0a5f27d5655bf235c401
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-05-18 22:52:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,34 +26,40 @@ window.onload = function () {
 
 
 function atualiza() {
-if(fim){
-    return
-}
+    if (fim) {
+        return
+        conteudo.fillStyle = "black"
+        conteudo.fillRect(0, 0, tela.width, tela.height)
+
+        conteudo.fillStyle = "red"
+        conteudo.fillRect(macaX, macaY, tamanho, tamanho)
+
+        if (cobraX == macaX && cobraY == macaY) {
+            cobra.push = ([macaX, macaY])
+            comida()
+        }
+
+    }
 }
 
-function mudaLocal(x){
-    if(x.code=="cima" && velocidadeY != -1){
+function mudaLocal(x) {
+    if (x.code == "cima" && velocidadeY != -1) {
         velocidadeX = 0
         velocidadeY = -1
-    }else if(x.code=="baixo" && velocidadeY != -1){
+    } else if (x.code == "baixo" && velocidadeY != -1) {
         velocidadeX = 0
         velocidadeY = 1
-    }else if(x.code=="esquerda" && velocidadeX != 1){
+    } else if (x.code == "esquerda" && velocidadeX != 1) {
         velocidadeX = -1
         velocidadeY = 0
-    }else if(x.code=="esquerda" && velocidadeX != 1){
+    } else if (x.code == "esquerda" && velocidadeX != 1) {
         velocidadeX = 1
         velocidadeY = 0
     }
 
 }
-function comida(){
-    macaX = Math.floor(Math.random()*colunas)*tamanho
-    macaY = Math.floor(Math.random()*colunas)*tamanho
+function comida() {
+    macaX = Math.floor(Math.random() * colunas) * tamanho
+    macaY = Math.floor(Math.random() * colunas) * tamanho
 }
 
-conteudo.fillStyle="black"
-conteudo.fillRect(0, 0, tela.width, tela.height)
-
-conteudo.fillStyle="red"
-conteudo.fillRect(macaX, macaY, tamanho, tamanho)
